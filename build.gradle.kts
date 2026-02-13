76e0f0524a752b94d6c4584ebc5d8893297fcf55
pochodnicky <pochodnicky@gratex.com>
2026-02-13 15:32:00 +0100

    Remove unused dependencies from build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,21 +19,6 @@ tasks.withType<ScalaCompile>().configureEach {
 dependencies {
 
     implementation("org.scala-lang:scala3-library_3:3.8.1")
-    implementation("org.antlr:antlr4-runtime:4.13.2")
-
-    implementation("org.l33tlabs.twl:pngdecoder:1.0")
-
-    implementation("org.lwjgl:lwjgl-glfw:3.4.1")
-    implementation("org.lwjgl:lwjgl-vulkan:3.4.1")
-    implementation("org.lwjgl:lwjgl-shaderc:3.4.1")
-
-    runtimeOnly("org.lwjgl:lwjgl:3.4.1:natives-linux")
-    runtimeOnly("org.lwjgl:lwjgl-glfw:3.4.1:natives-linux")
-    runtimeOnly("org.lwjgl:lwjgl-shaderc:3.4.1:natives-linux")
-
-    runtimeOnly("org.lwjgl:lwjgl:3.4.1:natives-windows")
-    runtimeOnly("org.lwjgl:lwjgl-glfw:3.4.1:natives-windows")
-    runtimeOnly("org.lwjgl:lwjgl-shaderc:3.4.1:natives-windows")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
