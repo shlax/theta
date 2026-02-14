47ac4ea5f0c488774679065f53e995f61a551f99
peter o <slax.10101@gmail.com>
2026-02-14 20:59:16 +0100

    Refactor Rule hierarchy by introducing Fact as a base class.

--- src/main/scala/org/theta/Binding.scala
+++ src/main/scala/org/theta/Binding.scala
@@ -8,33 +8,33 @@ object Binding {
 
 }
 
-class Binding(val arguments:Map[String, Variable], val onMatch: () => Unit, val parent:Option[Binding] = None) {
+class Binding(val state:Map[String, Variable], val onMatch: () => Unit, val parent:Option[Binding] = None) {
 
   def push(block: => Unit): Unit = {
-    val values = arguments.map { (key, value) =>
+    val values = state.map { (key, value) =>
       (key, value.value)
     }
 
     block
 
     for((key, value) <- values){
-      arguments(key).value = value
+      state(key).value = value
     }
   }
 
   def map(fn: => Unit): Binding = {
-    new Binding(arguments, { () => fn }, Some(this) )
+    new Binding(state, { () => fn }, Some(this) )
   }
 
   def merge(key:String, value: Value): Boolean = {
-    arguments.get(key) match {
+    state.get(key) match {
       case Some(v) => v.set(value)
       case None => false
     }
   }
 
   def solved: Boolean = {
-    arguments.forall( _._2.value.isDefined ) && parent.forall( _.solved)
+    state.forall( _._2.value.isDefined ) && parent.forall( _.solved )
   }
 
 }
