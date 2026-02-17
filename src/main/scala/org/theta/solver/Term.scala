9e209e1a7c65a91261d90fc084352d0bc825ce5a
pochodnicky <pochodnicky@gratex.com>
2026-02-17 13:23:18 +0100

    Refactor evaluation methods to return Boolean for execution control

--- src/main/scala/org/theta/solver/Term.scala
+++ src/main/scala/org/theta/solver/Term.scala
@@ -6,7 +6,7 @@ trait Term {
 
   def arguments:Set[String]
 
-  /** return true if execution should continue */
+  /** @return true if execution should continue */
   def evaluate(binding: Binding)(callback : => Boolean): Boolean
 
 }
