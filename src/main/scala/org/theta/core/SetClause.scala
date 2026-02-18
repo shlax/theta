e29cb5dd552ddf77fb875dfb56e03b94ea74e591
pochodnicky <pochodnicky@gratex.com>
2026-02-18 13:22:56 +0100

    Introduce `ForEachTerm` to support iterative term evaluation and remove redundant attributes from `Term` trait

--- src/main/scala/org/theta/core/SetClause.scala
+++ src/main/scala/org/theta/core/SetClause.scala
@@ -5,8 +5,7 @@ import org.theta.solver.{Binding, Value}
 class SetClause extends Operator("=", "x", "y"){
 
   override def evaluate(binding: Binding)(callback: => Boolean): Boolean = {
-    val x = binding("x")
-    val y = binding("y")
+    val x = binding("x"); val y = binding("y")
     if(x.isDefined && y.isDefined){
       if(x.resolve == y.resolve){
         callback
